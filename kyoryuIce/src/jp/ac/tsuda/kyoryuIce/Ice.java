f3717c5da435b6dba690c08f0abd109fadfa28d7
Rika Takashima <g13949tr@s20622.tsuda.ac.jp>
2015-06-01 16:26:59 +0900

    インポートした

---
+++ kyoryuIce/src/jp/ac/tsuda/kyoryuIce/Ice.java
@@ -0,0 +1,40 @@
+package jp.ac.tsuda.kyoryuIce;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import java.net.URL;
+
+@SuppressWarnings("serial")
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Ice implements Serializable{
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
+    @Persistent
+    private int number;
+    @Persistent
+	private String name;
+    @Persistent
+	private int price;
+	
+	public Ice(int number,String name,int price){
+		super();
+		this.number = number;
+		this.name = name;
+		this.price = price;
+	}
+	public Ice(){}
+	
+	public Long getId(){ return id; }
+	public int getNumber(){ return number; }
+	public String getName(){ return name; }
+	public int getPrice(){ return price; }
+	
+	
+}
